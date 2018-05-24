bbac4a255a3f53196bbd839bf460725ebca8a519
Tomoyuki Sakurai <240118+trombik@users.noreply.github.com>
2018-05-24 20:27:25 +0900

    [bugfix] hard-code all parameters supported by the module in the task (#4)
    * [bugfix] hard-code all parameters supported by the module in the task
    
    supposed to fix #3, but a bug in ansible 2.5.0 causes the test to fail.
    
    https://github.com/ansible/ansible/issues/37750
    
    * [bugfix] QA
    
    * [bugfix] follow a behaviour change in recent ansible, `failed` attribute always exists
    
    also, s/item./item.value/g
    
    * [bugfix] add more tests
    
    * [bugfix] update README

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -17,6 +17,24 @@ describe file("/etc/yum.repos.d/#{repo_name}.repo") do
   its(:content) { should match(/^\[#{ repo_name }\]\nenabled = 1\ngpgcheck = 1\nmirrorlist = #{ Regexp.escape('http://mirrors.fedoraproject.org/mirrorlist?repo=epel-7&arch=x86_64') }/) }
 end
 
+describe "Command yum repolist" do
+  let(:result) { command "yum repolist" }
+  it "exit status is zero" do
+    expect(result.exit_status).to eq 0
+  end
+  it "stderr is empty", retry: 3, retry_wait: 1 do
+    # XXX sometimes, stderr is not empty due to slow response time, and
+    # network glitch, retry if so
+    #
+    # XXX do not use `let`-ed, or cached, command result
+    expect(command("yum repolist").stderr).to eq ""
+  end
+
+  it "stdout shows the repository has been added" do
+    expect(result.stdout).to match(/^epel\s+epel repository\s+[0-9,]+$/)
+  end
+end
+
 package_not_in_default_repo.each do |p|
   describe command("yum install -y #{p}") do
     its(:stdout) do
